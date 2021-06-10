0df26ac94f81729fc5b3b1fc72e269e39665ddfc
CedricC <cedric-chape@live.fr>
2021-06-10 16:55:43 +0200

    add garage-spring-boot

---
+++ garage-spring-boot/src/main/java/fr/garage/GarageSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fr.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageSpringBootApplication.class, args);
+	}
+
+}
---
+++ garage-spring-boot/src/test/java/fr/garage/GarageSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
